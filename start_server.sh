bf2eb73e3a449a0fb6fa4f811697bd2d7eaf88d9
Timon Zimmermann <timonzimm@gmail.com>
2020-02-09 12:15:45 +0000

    Embeddings computed with max len 64

--- start_server.sh
+++ start_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash/
 export ZEROMQ_SOCK_TMP_DIR=/tmp/
 
-bert-serving-start -model_dir bert/uncased_L-24_H-1024_A-16/ -num_worker=4 -graph_tmp_dir /tmp/
+bert-serving-start -model_dir bert/uncased_L-24_H-1024_A-16/ -num_worker=2 -graph_tmp_dir /tmp/ -max_seq_len=64
